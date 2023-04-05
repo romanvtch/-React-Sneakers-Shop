4590801beca4e183791711c8cc137086641a1fcd
Roman Viitovych <114577315+romanvtch@users.noreply.github.com>
2023-04-05 13:01:43 +0200

    Update Header.jsx

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -9,7 +9,31 @@ export default function Header(props) {
  
   return (
     <header>
-     
+      <Link to="/">
+        <div className="headerLeft">
+          <img src="/img/header/logo.svg" alt="logo" />
+          <div>
+            <h3>React Sneakers</h3>
+            <p>Магазин найкращих кросівок</p>
+          </div>
+        </div>
+      </Link>
+      <ul className="headerRight">
+        <li onClick={props.onClickCart}>
+          <img className="cart" src="/img/header/shoppingCard.svg" alt="shoppingCard" />
+          <span>{totalPrice} грн.</span>
+        </li>
+        <li>
+          <Link to="favorites">
+            <img src="/img/header/favorites.svg" alt="favorites" />
+          </Link>
+        </li>
+        <li>
+          <Link to="orders">
+          <img src="/img/header/user.svg" alt="user" />
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 }
